2a028d08cc3a06d1f5f21a858d916b0dfa0edcea
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-16 15:46:28 -0400

    if_else_statement

---
+++ src/IfElseStatement.java
@@ -0,0 +1,19 @@
+public class IfElseStatement {
+    public static void main(String[] args){
+        int number = 10;
+        if (number > 0) {
+            System.out.println("Number is positive.");
+        }
+        else if (number == 0 ){
+            System.out.println("Number is not positive.");
+        }
+        else {
+            System.out.println("Application finished.");
+        }
+
+
+    }
+
+
+
+}
