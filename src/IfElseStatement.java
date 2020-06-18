e47a90ec802c7d09715c1ae1418055c914c5b1f3
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-18 08:52:01 -0400

    fixed the issue to take user input

--- src/IfElseStatement.java
+++ src/IfElseStatement.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class IfElseStatement {
     public static void main(String[] args){
-        int number = 10;
-        if (number > 0) {
-            System.out.println("Number is positive.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int number = input.nextInt();
+        if (number > 10) {
+            System.out.println("Number entered is greater than 10");
         }
-        else if (number == 0 ){
-            System.out.println("Number is not positive.");
+        else if (number == 10 ){
+            System.out.println("Number entered is equal to 10. ");
         }
         else {
-            System.out.println("Application finished.");
+            System.out.println("Number entered is less than 10. ");
         }
 
 
